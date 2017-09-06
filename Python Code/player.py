3c976d6f676b0e1fd3cb38878d89ceb36e604980
Ferdinand Silva <ferdinandsilva@ferdinandsilva.com>
2017-09-06 14:04:02 +0800

    remote

--- Python Code/player.py
+++ Python Code/player.py
@@ -16,9 +16,9 @@ class RARemote(Thread):
     VOLUME_UP = "FD807F"
     VOLUME_DOWN = "FD906F"
 
-    def __init__(self, player):
+    def __init__(self):
         super(RARemote, self).__init__()
-        self.player = player
+        self.isPlaying = False
         self.serial = serial.Serial(SERIAL_PORT, SERIAL_BAUD_RATE)
     
     def run(self):
@@ -29,78 +29,20 @@ class RARemote(Thread):
             msg = self.serial.readline()
             
             if msg == RARemote.PLAY_PAUSE:
-                self.player.playPause()
+                if self.isPlaying:
+                    subprocess.Popen("echo -n p >/tmp/mp3player", shell=True)
+                else:
+                    subprocess.Popen("echo -n . >/tmp/mp3player", shell=True)
+                    self.isPlaying = True
             elif msg == RARemote.REWIND:
-                self.player.rewind()
+                subprocess.Popen("echo -n $'\x1b\x5b\x44' > /tmp/mp3player", shell=True)
             elif msg == RARemote.FORWARD:
-                self.player.forward()
+                subprocess.Popen("echo -n $'\x1b\x5b\x43' > /tmp/mp3player", shell=True)
             elif msg == RARemote.VOLUME_UP:
-                self.player.volume_up()
+                subprocess.Popen("echo -n + > /tmp/mp3player", shell=True)
             elif msg == RARemote.VOLUME_DOWN:
-                self.player.volume_down()
-
-class RAMP3PlayerRunner(Thread):
-
-    def __init__(self, process):
-        super(RAMP3PlayerRunner, self).__init__()
-        self.process = process
-
-    def run(self):
-        self.process.wait()
-
-class RAMP3Player(object):
-
-    def __init__(self, filename):
-        self.filename = filename
-        self.playerProcess = None
-        self.isPlaying = False
-        self.__initPlayer()
-
-    def rewind(self):
-        if self.checkRunning():
-            print "rewind"
-            subprocess.Popen("echo -n $'\x1b\x5b\x44' > /tmp/mp3player", shell=True)
-
-    def forward(self):
-        if self.checkRunning():
-            subprocess.Popen("echo -n $'\x1b\x5b\x43' > /tmp/mp3player", shell=True)
-
-    def volume_up(self):
-        if self.checkRunning:
-            subprocess.Popen("echo -n + > /tmp/mp3player", shell=True)
-
-    def volume_down(self):
-        if self.checkRunning:
-            subprocess.Popen("echo -n - > /tmp/mp3player", shell=True)
-
-    def playPause(self):
-        if self.checkRunning():
-            if self.isPlaying:
-                self.isPlaying = False
-                subprocess.Popen("echo -n p >/tmp/mp3player", shell=True)
-            else:
-                subprocess.Popen("echo -n . >/tmp/mp3player", shell=True)
-                self.isPlaying = True
-
-    def __initPlayer(self):
-        mkfifo = subprocess.Popen("mkfifo /tmp/mp3player", shell=True)
-        mkfifo.wait()
-        self.playerProcess = subprocess.Popen("omxplayer %s < /tmp/mp3player" % self.filename, shell=True)
-        subprocess.Popen("echo -n . >/tmp/mp3player", shell=True) #start right away
-        """
-        self.isPlaying = True
-        runner = RAMP3PlayerRunner(self.playerProcess)
-        runner.start()
-        remote = RARemote(self)
-        remote.start()
-        """
-
-    def checkRunning(self):
-        poll = self.playerProcess.poll()
-
-        if poll is None:
-            return True
-        return False
+                subprocess.Popen("echo -n - > /tmp/mp3player", shell=True)
 
 if __name__ == "__main__":
-    player = RAMP3Player("asin.mp3")
+    remote = RARemote()
+    remote.start()
